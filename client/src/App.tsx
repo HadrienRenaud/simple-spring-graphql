f496c57ac1b997809f761edd0df1a2185340b85a
Hadrien RENAUD <hadrien.renaud@polytechnique.edu>
2019-08-15 14:13:17 +0200

    ➕ Add subscription  material ✨

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,49 @@
 import React from 'react';
-import './App.css';
+
+import { ApolloProvider } from '@apollo/react-hooks';
+import { InMemoryCache } from 'apollo-boost';
+import ApolloClient from 'apollo-client';
+import { split } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+
+import { Sender } from './components/Sender/Sender'
+import { Stream } from './components/Stream/Stream';
+
+// Create an http link:
+const httpLink = new HttpLink({
+  uri: 'http://localhost:8080/graphql'
+});
+
+// Create a WebSocket link:
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:8080/graphql`,
+  options: {
+    reconnect: true
+  }
+});
+
+// using the ability to split links, you can send data to each link
+// depending on what kind of operation is being sent
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
+const client = new ApolloClient({
+    // Provide required constructor fields
+    cache: new InMemoryCache(),
+    link: link,
+  });
 
 const Footer = () => {
     return <>
@@ -11,13 +55,9 @@ const Footer = () => {
 const App: React.FC = () => {
     return (
         <div className="App">
-            <div app-content>
-                Coucou
-            </div>
-
-            <div className="footer">
-                <Footer/>
-            </div>
+            <ApolloProvider client={client}>
+                <Stream />
+            </ApolloProvider>
         </div>
     );
 }
