0f2fa159e5965f1a9aa8ea68310d7794e466037e
Hadrien RENAUD <hadrien.renaud@polytechnique.edu>
2019-08-16 16:13:16 +0200

    feat(stream):  use apollo subscribeToMore

--- client/src/components/Stream/Stream.tsx
+++ client/src/components/Stream/Stream.tsx
@@ -1,54 +1,114 @@
-import React, { useState } from 'react';
+import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { ApolloError } from "apollo-client";
+import gql from "graphql-tag";
 
-import gql from 'graphql-tag';
-import { useSubscription } from '@apollo/react-hooks';
+import { StreamMessage } from "./StreamMessage";
 
-import { StreamMessage } from './StreamMessage'
-
-const STREAM_MESSAGES = gql`
-subscription newMessage {
-  newMessages {
-    content
-    createdAt
-    author {
-      username
+const LIST_MESSAGES = gql`
+  query allMessages {
+    allMessages {
+      id
+      content
+      createdAt
+      author {
+        username
+      }
     }
   }
-}
-`
+`;
+
+const STREAM_MESSAGES = gql`
+  subscription newMessage {
+    newMessages {
+      id
+      content
+      createdAt
+      author {
+        username
+      }
+    }
+  }
+`;
 
 export interface Message {
-  content: string,
-  createdAt: Date,
+  id?: number;
+  content: string;
+  createdAt: Date;
   author?: {
-    username: string
-  }
+    username: string;
+  };
+}
+
+interface QueryData {
+  allMessages: Array<Message>;
 }
 
 interface SubscriptionData {
-  newMessages: Message
+  newMessages: Message;
+}
+
+interface DisplayProps {
+  messages: Array<Message>;
+  loading: boolean;
+  error?: ApolloError;
+  subscribeToNewMessages: () => void;
+}
+
+export class DisplayMessages extends React.Component<DisplayProps> {
+  componentDidMount() {
+    this.props.subscribeToNewMessages();
+  }
+
+  render = () => {
+    const { messages, loading, error } = this.props;
+    return (
+      <>
+        {messages.map((message, index) => (
+          <StreamMessage key={index} message={message} />
+        ))}
+        {loading && <p>Loading ...</p>}
+        {error && <p>Error: {error.message}</p>}
+      </>
+    );
+  };
 }
 
 export function Stream() {
-  const initialValue: Array<Message> = [{
-    content: "Bienvenue !",
-    createdAt: new Date()
-  }];
-  const [messages, setMessages] = useState(initialValue)
-  const { loading, error} = useSubscription<SubscriptionData>(STREAM_MESSAGES, {
-    onSubscriptionData: (hookArg) => {
-      if (hookArg.subscriptionData.data) {
-        setMessages([ ...messages, hookArg.subscriptionData.data.newMessages ])
+  const { loading, error, data, subscribeToMore } = useQuery<QueryData>(
+    LIST_MESSAGES
+  );
+
+  let messages: Array<Message> = [];
+  if (data && data.allMessages) {
+    messages = data.allMessages.sort((m1: Message, m2: Message) => {
+      const d1 = new Date(m1.createdAt).getTime();
+      const d2 = new Date(m2.createdAt).getTime();
+      return d1 - d2;
+    });
+  }
+
+  return (
+    <DisplayMessages
+      loading={loading}
+      error={error}
+      messages={messages}
+      subscribeToNewMessages={() =>
+        subscribeToMore<SubscriptionData>({
+          document: STREAM_MESSAGES,
+          updateQuery: (prev, { subscriptionData }) => {
+            if (subscriptionData.data) {
+              const newMessage = subscriptionData.data.newMessages;
+              return {
+                ...prev,
+                allMessages: [...prev.allMessages, newMessage]
+              };
+            } else {
+              return prev;
+            }
+          }
+        })
       }
-    }
-  });
-
-  return <>
-    {messages.map((message, index) => (
-      <StreamMessage key={index} message={message} />
-    ))}
-    {loading && <p>Loading ...</p>}
-    {error && <p>Error: {error.message}</p>}
-  </>;
+    />
+  );
 }
-
